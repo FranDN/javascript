d99f5ccbea64b1be0180369735b73bc58495ff9d
François Dive <divefrancois@gmail.com>
2025-05-15 13:50:17 +0200

    still messing

--- javascript.js
+++ javascript.js
@@ -10,3 +10,17 @@ console.log (age);
 age = 54;
 
 console.log (age);
+
+const pi = 3.14;
+
+console.log (pi);
+
+console.log (3+2) - 76 * (1+1)
+
+console.log(23+97);
+
+console.log(1+2+4+6+8+9);
+
+console.log((4+6+9)/77);
+
+let

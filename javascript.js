60f3590135585b9a549b18bb37723f7e8d0c9ec3
François Dive <divefrancois@gmail.com>
2025-05-15 13:56:59 +0200

    playing and messing

--- javascript.js
+++ javascript.js
@@ -23,4 +23,19 @@ console.log(1+2+4+6+8+9);
 
 console.log((4+6+9)/77);
 
-let
+let a = 10;
+console.log(a);
+
+a = 47;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);

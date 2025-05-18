c49304ca4205198b35c4d4c7fa7007cc25be310c
François Dive <divefrancois@gmail.com>
2025-05-18 18:32:01 +0200

    msessingfjzoj

--- javascript.js
+++ javascript.js
@@ -42,3 +42,10 @@ console.log(percentage);
 
 const string = "The revolution will not be televised."
 console.log(string);
+
+const badString1 = This is a test;
+const badString2 = 'This is a test;
+const badString3 = This is a test';
+
+const badString = string;
+console.log(badString);

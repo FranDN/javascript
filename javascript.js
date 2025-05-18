b321bcc1a55088a1d3437b5719ed843c7563f282
François Dive <divefrancois@gmail.com>
2025-05-18 18:33:21 +0200

    messsssin

--- javascript.js
+++ javascript.js
@@ -45,3 +45,11 @@ console.log(string);
 
 const badString = string;
 console.log(badString);
+
+const single = 'Single quotes';
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
